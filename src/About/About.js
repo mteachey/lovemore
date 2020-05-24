16f62825d2b84195948900e9223ac28f5faee98c
mteachey <melindateachey@gmail.com>
2020-05-23 18:26:23 -0600

    fixed image sizing issue

--- src/About/About.js
+++ src/About/About.js
@@ -8,8 +8,8 @@ class About extends Component{
                     <h2>What is Love More Laugh More?</h2>
                 </header>
                 <main id="about">
-                    <p>Love More Laugh More is place to motivate and inspire you to care for yourself more, so you can...well, so you can do all sorts of things, including care for others more.</p><p>When you are living your best and most joyful life, it is easier to help others - loved ones and strangers you interact with - live theirs.</p>
-                    <p>This app is to help you plan time and motivate yourself to show yourself some love and care every day.</p>
+                    <p>Love More Laugh More is place to motivate and inspire you to care for yourself more, so you can...well, so you can do all sorts of things, including care for others more.</p><p>When you are living your best and most joyful life, it is easier to help others live theirs.</p>
+                    <p>This app hopes to help you plan time and motivate to show yourself some love and care every day.</p>
                     <p>This app provides a place for you to set goals, see and celebrate your progress, and find ideas for inspiration. We have also included an online gratitude journal - because the more you can appreciate what you already have, the more you can love yourself and the whole world.</p>
                     <p>Love more laugh more</p>
                 </main>
